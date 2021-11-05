f75fe2db4fdca559070f475d2d73abc80ed6a3b4
Irek Nurtdinov <nurtdinov.irek@gmail.com>
2021-11-05 12:02:21 +0300

    add if construction

---
+++ Repo/Program.cs
@@ -0,0 +1,70 @@
+﻿// Console.Write("a = ");
+// Console.Write(a);
+// Console.Write("b = ");
+// Console.WriteLine(b);
+//Console.Write("Hello");
+
+// Console.WriteLine("a= " + a + "\nb= " + b);  //конкатинация
+// int a = 123;
+// int b = 321;
+// Console.WriteLine($"Было   a = {a} b = {b}"); // a = 123, b = 321
+
+// int c;
+// c = a;
+// a = b;
+// b = c;
+
+// a = a+b;
+// b = a-b;
+// a=a-b;
+
+// Console.WriteLine($"Стало  a = {a} b = {b}"); // a = 321, b = 123
+
+// int a = 5;
+// int b = 2;
+// int res = a & b;
+// Console.WriteLine($"res = {res}");
+
+// if(условие)
+// {
+// // условие истина
+// }
+// else()
+// {
+// // условие ложно
+// }
+
+int a = 3;
+int b = 4;
+
+if(a>b)
+{
+Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
+// void DrawRectanglle(int posX, int posY, int width, int height)
+// {
+//     int row = 0;
+//     while (row < height)
+//     {
+//         Console.SetCursorPosition(posX, posY + row);
+//         int count = 0;
+//         while (count < width)
+//         {
+//             Console.Write("+");
+//             count++;
+//         }
+//         row++;
+//     }
+// }
+// DrawRectanglle(5, 10, 9, 4);
+// int i = 0;
+
+// while (i<10)
+// {
+//      i += 2;
+//      Console.WriteLine(i);
+// }
